c4dbd8bf260597ca6446b8fa03ec55dade522dc1
Johnson-huang <2678683520@qq.com>
2022-05-29 02:45:58 +0800

    新增方法

--- src/modules/dateFormat.ts
+++ src/modules/dateFormat.ts
@@ -1,11 +1,27 @@
-export function formatDate (dateTime, fmt = 'yyyy-MM-dd HH:mm:ss') {
-    let date = ''
-    switch (typeof dateTime) {
+/**
+ * 日期格式化
+ * 传入 new Date() 兼容的入参，兼容 IOS Date - 不识别问题
+ * @description
+ * yyyy：年
+ * MM：月
+ * dd：日
+ * HH：时（24小时制）
+ * hh：时（12小时制）
+ * mm：分
+ * ss：秒
+ * @param param {number|string}
+ * @param fmt {string}
+ * @returns {string}
+ * @example dateFormat(1653763181201) =>
+ */
+export function dateFormat (param: number|string, fmt = 'yyyy-MM-dd HH:mm:ss'): string {
+    let date
+    switch (typeof param) {
         case 'string':
-            date = new Date(Date.parse(dateTime.replace(/-/g, '/')))
+            date = new Date(Date.parse(param.replace(/-/g, '/')))
             break
         default:
-            date = new Date(dateTime)
+            date = new Date(param)
             break
     }
     let yyyy = date.getFullYear()
@@ -16,7 +32,7 @@ export function formatDate (dateTime, fmt = 'yyyy-MM-dd HH:mm:ss') {
     let ss = date.getSeconds()
     let SSS = date.getMilliseconds()
     let hh = HH > 12 ? HH - 12 : HH
-    let dateStr = fmt
+    return fmt
         .replace('yyyy', String(yyyy))
         .replace('MM', String(MM + 1).padStart(2, '0'))
         .replace('dd', String(dd).padStart(2, '0'))
@@ -25,5 +41,4 @@ export function formatDate (dateTime, fmt = 'yyyy-MM-dd HH:mm:ss') {
         .replace('ss', String(ss).padStart(2, '0'))
         .replace('hh', String(hh).padStart(2, '0'))
         .replace('SSS', String(SSS).padStart(3, '0'))
-    return dateStr
 }
