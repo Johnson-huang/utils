d5bb950fb64ae68e4defdb8bd3f7db6cc5d90a21
Johnson-huang <2678683520@qq.com>
2022-06-01 00:14:30 +0800

    新增方法

--- src/modules/keepDecimals.ts
+++ src/modules/keepDecimals.ts
@@ -3,19 +3,44 @@ import divide from "./divide";
 import {keep_decimals_type} from "../interface";
 
 /**
- * 保留数字小数位数
+ * 保留数字小数位数（截取 or 四舍五入）
+ *
+ * JS 原生的四舍五入用的是银行舍入原则
+ * 简单来说：五后非零就进一，五后为零看五前奇偶，五前为偶应舍去，五前为奇要进一
+ * 即 2.045 -> 2.04；2.035 -> 2.04
  * @param num
  * @param decimals 保留的小数位数
  * @param type 保留方式 toFixed：四舍五入 slice：截取
- * @example
- * keepDecimals(0.12567) => '0.13'
- * keepDecimals(0.12345, 3, 'slice') => '0.123'
- * keepDecimals(0.12, 3, 'slice') => '0.120'
+ * @example keepDecimals(0.12567) => '0.13'
+ * @example keepDecimals(0.12345, 3, 'slice') => '0.123'
+ * @example keepDecimals(0.12, 3, 'slice') => '0.120'
  */
 export default function keepDecimals(num: number, decimals = 2, type = keep_decimals_type.TO_FIXED): string {
+    const bigNumStr = multiply(num, Math.pow(10, decimals)) + ''
+    const list = bigNumStr.split('.')
+    // 截取 或 四舍五入 处理
+    let afterDealNum
     if (type === keep_decimals_type.SLICE) {
-        return (divide(Math.floor(multiply(num, Math.pow(10, decimals))), Math.pow(10, decimals)) + '').padEnd(decimals, '0')
+        // 截取后数字
+        afterDealNum = divide(+list[0], Math.pow(10, decimals))
     } else {
-        return num.toFixed(decimals)
+        // 按照 decimals 四舍五入后的数字
+        if (list[1] && +list[1][(list[1] as string).length - 1] >= 5) {
+            afterDealNum = divide((+list[0] >= 0 ? +list[0] + 1 : +list[0] - 1), Math.pow(10, decimals))
+        } else {
+            afterDealNum = divide(+list[0], Math.pow(10, decimals))
+        }
     }
+    // 补零
+    const afterDealNumStr = afterDealNum + ''
+    const afterList = afterDealNumStr.split('.')
+    let decimalsStr = ''
+    if (afterList[1]) {
+        if (afterList[1].length >= decimals) {
+            decimalsStr = afterList[1].slice(0, decimals + 1)
+        } else {
+            decimalsStr = afterList[1].padEnd(decimals, '0')
+        }
+    }
+    return `${afterList[0]}${decimalsStr ? '.' + decimalsStr : decimalsStr}`
 }
--- test/keepDecimals.spec.ts
+++ test/keepDecimals.spec.ts
@@ -10,11 +10,19 @@ describe('测试 keepDecimals.ts', () => {
         expect(keepDecimals(0.125, 2)).toBe('0.13')
     })
 
+    it('负数四舍五入', () => {
+        expect(keepDecimals(-0.125, 2)).toBe('-0.13')
+    })
+
     it('截取', () => {
         expect(keepDecimals(0.12345, 3, keep_decimals_type.SLICE)).toBe('0.123')
     })
 
-    it('补零', () => {
+    it('原数小数位数小，四舍五入补零', () => {
         expect(keepDecimals(0.1, 3)).toBe('0.100')
+    })
+
+    it('原数小数位数小，截取补零', () => {
+        expect(keepDecimals(0.1, 3, keep_decimals_type.SLICE)).toBe('0.100')
     })
 })
